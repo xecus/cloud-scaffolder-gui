a835aeb27720c76a6f4de74847f0ad417ad515fb
Hiroyuki Ootaguro <hiroyuki@abeja.asia>
2017-01-13 20:08:09 +0000

    remove waste codes

--- src/main.js
+++ src/main.js
@@ -34,12 +34,6 @@ new Vue({
     toggleLeftSidenav () {
       this.$refs.leftSidenav.toggle()
     },
-    toggleRightSidenav () {
-      this.$refs.rightSidenav.toggle()
-    },
-    closeRightSidenav () {
-      this.$refs.rightSidenav.close()
-    },
     open (ref) {
       console.log('Opened: ' + ref)
     },
